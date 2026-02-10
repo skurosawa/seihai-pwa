94dde973967e802185321837053c42072ab70b89
Shinsuke Kurosawa <veloiuria00@gmail.com>
2026-02-10 18:22:43 +0900

    fix base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,24 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  base: "/seihai-pwa/",
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Seihai",
+        short_name: "Seihai",
+        start_url: "/seihai-pwa/",
+        scope: "/seihai-pwa/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        icons: [
+          { src: "pwa-192.png", sizes: "192x192", type: "image/png" },
+          { src: "pwa-512.png", sizes: "512x512", type: "image/png" }
+        ]
+      }
+    }),
+  ],
 });
